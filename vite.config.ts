5878c5596557998c436df8862aa0b7d93a3bf5b5
huanjay6-cyber <huanjay6@gmail.com>
2026-08-12 11:40:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-  },
+    base: "./",
+    plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+    },
 });
